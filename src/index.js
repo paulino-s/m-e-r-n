8700675a75a0534a55f8f79acf239b099e82d2ea
PaulinoVasquez <paulinovasquez900@gmail.com>
2021-07-01 23:14:18 -0600

    configure server

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const   express = require('express')
+const   app = express()
+const   morgan = require('morgan')
+const   cors = require('cors')
+const bodyparser = require('body-parser')
+
+app.set('Port', 4000)
+
+app.listen(app.get('Port'), ()      =>{
+    console.log('Escuchando en el puerto', app.get('Port'))
+});
